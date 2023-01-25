a97bf0dad48487486d647a032e25313e4fe0a8e7
MetelM <metelskymatvey@gmail.com>
2023-01-25 16:17:42 +0300

    second commit2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,12 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
+
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Александр","Розенбаум", (byte)50);
         userService.saveUser("Григорий","Лепс", (byte) 50);
